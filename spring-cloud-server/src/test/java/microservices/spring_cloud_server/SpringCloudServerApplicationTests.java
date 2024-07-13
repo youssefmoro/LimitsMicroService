6285e8aa50141f6190071afd4bea5d9f3428e3fa
Yousef Moro <36382324+youssefmoro@users.noreply.github.com>
2024-07-13 16:35:40 +0300

    Add files via upload

---
+++ spring-cloud-server/src/test/java/microservices/spring_cloud_server/SpringCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.spring_cloud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
